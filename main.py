91479b85b1cf709da2f43007059afc0d743cfe46
dkncus <dkncus@gmail.com>
2020-11-27 12:26:58 -0500

    Github API Access Init Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from github import Github
+
+g = Github()
+
+username = "phadej"
+
+user = g.get_user(username)
+
+repos = user.get_repos()
+total_repos = 0
+for repo in repos:
+    total_repos = total_repos + 1
+
+print("Github User Statistics:", username)
